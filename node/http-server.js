70e6e51e96ae0d2fb2e956f3a2294c45ad001ee2
libra guest <jmetaj@hotmail.com>
2012-09-26 16:40:55 -0400

    Corrected Jade use, layouts. added package.json

--- node/http-server.js
+++ node/http-server.js
@@ -1,6 +1,6 @@
 // Original from http://stackoverflow.com/questions/10216395/error-failed-to-lookup-view-in-express
 var express = require('express');
-var app = module.exports = express.createServer();
+var app = module.exports = express();
 
 require('./config/environment.js')(app, express);
 require('./config/routes.js')(app);
